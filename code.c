d98ccc4ca94265bf0dd0dbdd1b444c1940f83cea
Anton-Leonov-IKBO-03-17 <oh22@mail.ru>
2017-11-03 15:54:01 +0300

    Update code.c

--- code.c
+++ code.c
@@ -1,1 +1,7 @@
-
+#include <stdio.h>
+int main(int argc, char* argv[]){
+    while(fopen(argv[1],r)== NULL){
+        printf("This program needs file, please, provide it with an argument of command line");
+        
+    }
+}
