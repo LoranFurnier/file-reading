bb8532d2964ba03b64a7b5d66df27eb5a27e92b4
Anton-Leonov-IKBO-03-17 <oh22@mail.ru>
2017-11-03 16:02:08 +0300

    Update code.c

--- code.c
+++ code.c
@@ -1,7 +1,13 @@
 #include <stdio.h>
+#include <string.h>
 int main(int argc, char* argv[]){
-    while(fopen(argv[1],r)== NULL){
+    FILE *file;
+    char *fname = argv[1];
+    char str[255];
+    while(fopen(argv[1],r) == NULL){
+        file = fopen(argv[1], r);
         printf("This program needs file, please, provide it with an argument of command line");
-        
+        return 0;
     }
+    fclose(file);
 }
