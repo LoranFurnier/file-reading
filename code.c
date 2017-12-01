619aa9eadf7bc7c9a5cd8813e65aea32ffe86858
Anton-Leonov-IKBO-03-17 <oh22@mail.ru>
2017-12-01 16:10:31 +0300

    Update code.c

--- code.c
+++ code.c
@@ -1,15 +1,13 @@
 #include <stdio.h>
-int main(int argc, char *argv[]){
-	FILE *file; 
-	int bytes, num1;
-	float num2;
-	char *fname = argv[1];
-	char string[255];
-	file = fopen(fname,"rb");
-	while(file == NULL)
-	{
-		printf("не могу открыть файл '%s'",fname);
-		return 0;
-	}
-	/*число с плавающей точкой занимает 8 байт, целое число - 8 байт, строка - по 1 байту на символ*/
+int main() {
+    FILE *fp;
+    char *name;
+    scanf("%s", name);
+    if((fp = fopen(name, "r"))!=NULL) { // открыть файл удалось?
+        while (fgetc(fp)!='\n'){
+            fgetc(fp);
+        }
+    } else printf("Не удалось открыть файл");
+    fclose(fp);
+    return 0;
 }
