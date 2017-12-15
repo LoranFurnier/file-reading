3c90ac17713b1259bec1ca3e6c12ef5c584c3fcc
Anton-Leonov-IKBO-03-17 <oh22@mail.ru>
2017-12-15 16:10:38 +0300

    Update code.c

--- code.c
+++ code.c
@@ -3,12 +3,16 @@
 int main(int argc, char *argv[])
 {
    char *str;
-   struct pack{
+   struct p{
        int numi;
        char *str2;
        float numf;
-   } pack;
+   }p;
    int i;
+   int numi;
+   char c;
+   char *str2;
+   float numf;
    char *path;
    size_t result;
    int k = 0;
@@ -22,25 +26,22 @@ int main(int argc, char *argv[])
      scanf("%s", &path);
      f1 = fopen(&path, "rb");
    }
-   
-   fseek(f1, 0 , SEEK_END);  
+
+   fseek(f1, 0 , SEEK_END);
    long lSize = ftell(f1);
-   rewind (f1); 
-   
+   rewind (f1);
+
    str = (char*) malloc(sizeof(char) * lSize);
   if (str == NULL)
   {
       printf("– COMRADE COLONEL, THERE'S SOMETHING WRONG! SHOOT?\n– Exactly.");
       exit(2);
   }
-  do{
-      fread(pack.numi, sizeof(int), 1, f1);
-      fread(pack.str2, sizeof(pack.str2), 1, f1);
-      fread(pack.numf, sizeof(float), 1, f1);
-      printf("%d %s %f", pack.numi, pack.str2, pack.numf);
-  }while(!feof(f1));
-  
-  printf("\nstr = %s\n", str);
+
+    while((c=getc(f1))!= EOF)
+    {
+    printf("%c", c);
+    }
   printf("Comrade Colonel, there'd been found %d strings and %d bytes of information! Shoot? \n", k, lSize);/*Written 100 records, total 26400 bytes*/
   fclose (f1);
    return 0;
